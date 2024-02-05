7b0b50820680fcaf068bd4a6a7a136df7f3194e8
SergeyAnshin <anshin.serega@gmail.com>
2024-02-06 00:39:04 +0300

    create RateController, impl getExternalApiCurrencyRateByDate()

---
+++ src/main/java/org/good/job/currency/project/controller/RateController.java
@@ -0,0 +1,36 @@
+package org.good.job.currency.project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.good.job.currency.project.entity.GeneralRate;
+import org.good.job.currency.project.entity.enums.ExternalApiName;
+import org.good.job.currency.project.service.impl.RateService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.Currency;
+
+
+@RequiredArgsConstructor
+
+@Validated
+
+@RestController
+@RequestMapping("/rate")
+public class RateController {
+
+    private final RateService rateService;
+
+    @GetMapping
+    public ResponseEntity<GeneralRate> getExternalApiCurrencyRateByDate(@RequestParam ExternalApiName externalApiName,
+                                                                        @RequestParam Currency currency,
+                                                                        @RequestParam LocalDate date) {
+        var rate = rateService.getRateByExternalApiNameAndCurrencyAndDate(externalApiName, currency, date);
+        return ResponseEntity.ok(rate);
+    }
+
+}
