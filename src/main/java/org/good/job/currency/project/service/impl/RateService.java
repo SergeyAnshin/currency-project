3ccc72d88ed96862ee659f06eecdc75f79b64ad2
SergeyAnshin <anshin.serega@gmail.com>
2024-02-06 00:30:34 +0300

    impl getRateByExternalApiNameAndCurrencyAndDate()

---
+++ src/main/java/org/good/job/currency/project/service/impl/RateService.java
@@ -0,0 +1,41 @@
+package org.good.job.currency.project.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.good.job.currency.project.entity.ExternalApiUrl;
+import org.good.job.currency.project.entity.GeneralRate;
+import org.good.job.currency.project.entity.enums.ExternalApiName;
+import org.good.job.currency.project.service.ExternalApiCaller;
+import org.good.job.currency.project.service.ExternalApiDtoMapper;
+import org.good.job.currency.project.service.ExternalApiUrlService;
+import org.good.job.currency.project.service.mapper.RateMapper;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Currency;
+
+
+@RequiredArgsConstructor
+
+@Service
+public class RateService {
+
+    private final ExternalApiCaller externalApiCaller;
+    private final ExternalApiUrlService urlService;
+    private final RateMapper rateMapper;
+    private final ExternalApiDtoMapper externalApiDtoMapper;
+
+    //TODO обработка rate list
+    public GeneralRate getRateByExternalApiNameAndCurrencyAndDate(ExternalApiName externalApiName,
+                                                                  Currency currencyCode,
+                                                                  LocalDate date) {
+        var param = ExternalApiUrl.builder().externalApiName(externalApiName).currency(currencyCode).date(date).build();
+
+        var externalApiRateUrl = urlService.generateRateUrlByExternalApiNameAndCurrencyAndDate(param);
+        var responseBody = externalApiCaller.call(externalApiRateUrl);
+
+        var dtoClass = externalApiName.getExternalApiRateProperty().getRateProperty().getDtoClass();
+        var externalApiDto = externalApiDtoMapper.responseBodyToExternalApiDto(responseBody, dtoClass);
+        return rateMapper.externalApiRateDtoToRate(externalApiDto);
+    }
+
+}
