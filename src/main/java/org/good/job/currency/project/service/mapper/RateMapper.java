5664b6d64994e7e44c0fd0211411ca517ccc1932
SergeyAnshin <anshin.serega@gmail.com>
2024-02-07 02:49:32 +0300

    add @SubclassMapping for alfa and belarus banks

--- src/main/java/org/good/job/currency/project/service/mapper/RateMapper.java
+++ src/main/java/org/good/job/currency/project/service/mapper/RateMapper.java
@@ -1,5 +1,7 @@
 package org.good.job.currency.project.service.mapper;
 
+import org.good.job.currency.project.dto.AlfaBankRate;
+import org.good.job.currency.project.dto.BelarusBankRate;
 import org.good.job.currency.project.dto.NbrbRateDto;
 import org.good.job.currency.project.entity.GeneralRate;
 import org.mapstruct.Mapper;
@@ -10,6 +12,8 @@ import org.mapstruct.SubclassMapping;
 public interface RateMapper {
 
     @SubclassMapping(target = GeneralRate.class, source = NbrbRateDto.class)
+    @SubclassMapping(target = GeneralRate.class, source = AlfaBankRate.class)
+    @SubclassMapping(target = GeneralRate.class, source = BelarusBankRate.class)
     GeneralRate externalApiRateDtoToRate(Object externalApiRateDto);
 
 }
