573a7ca0005f11fb6f24af7635b4401ab3e11298
SergeyAnshin <anshin.serega@gmail.com>
2024-02-07 02:55:54 +0300

    create BelarusBankRateList dto

---
+++ src/main/java/org/good/job/currency/project/dto/BelarusBankRateList.java
@@ -0,0 +1,27 @@
+package org.good.job.currency.project.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+@ToString
+@JsonDeserialize(using = BelarusBankRateListDeserializer.class)
+public class BelarusBankRateList implements Foreachable<BelarusBankRate> {
+
+    List<BelarusBankRate> rates;
+
+    @Override
+    public List<BelarusBankRate> getListDto() {
+        return rates;
+    }
+
+
+}
