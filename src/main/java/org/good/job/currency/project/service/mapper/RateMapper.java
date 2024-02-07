2f63fcb9821e419b061b20a5f7acdf2c15dad000
SergeyAnshin <anshin.serega@gmail.com>
2024-02-07 16:53:12 +0300

    add mapping customization for BelarusBankRate

--- src/main/java/org/good/job/currency/project/service/mapper/RateMapper.java
+++ src/main/java/org/good/job/currency/project/service/mapper/RateMapper.java
@@ -3,17 +3,19 @@ package org.good.job.currency.project.service.mapper;
 import org.good.job.currency.project.dto.AlfaBankRate;
 import org.good.job.currency.project.dto.BelarusBankRate;
 import org.good.job.currency.project.dto.NbrbRateDto;
+import org.good.job.currency.project.entity.ExternalApiUrl;
 import org.good.job.currency.project.entity.GeneralRate;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.SubclassMapping;
 
 
-@Mapper
+@Mapper(uses = { BelarusBankRateMapper.class })
 public interface RateMapper {
 
     @SubclassMapping(target = GeneralRate.class, source = NbrbRateDto.class)
     @SubclassMapping(target = GeneralRate.class, source = AlfaBankRate.class)
     @SubclassMapping(target = GeneralRate.class, source = BelarusBankRate.class)
-    GeneralRate externalApiRateDtoToRate(Object externalApiRateDto);
+    GeneralRate externalApiRateDtoToRate(Object externalApiRateDto, @Context ExternalApiUrl externalApiUrl);
 
 }
