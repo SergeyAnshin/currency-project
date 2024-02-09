7183fda367c68ac2a199a2593af1cdf9b85f9322
SergeyAnshin <anshin.serega@gmail.com>
2024-02-09 11:46:27 +0300

    change currencySupportedByExternalApi check

--- src/main/java/org/good/job/currency/project/service/validation/SupportedExternalApiAndCurrencyValidator.java
+++ src/main/java/org/good/job/currency/project/service/validation/SupportedExternalApiAndCurrencyValidator.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraintvalidation.SupportedValidationTarget;
 import jakarta.validation.constraintvalidation.ValidationTarget;
 import lombok.RequiredArgsConstructor;
 import org.good.job.currency.project.entity.enums.ExternalApiName;
-import org.good.job.currency.project.service.RateService;
+import org.good.job.currency.project.service.CurrencyService;
 import org.good.job.currency.project.service.validation.annotation.SupportedCurrencyByExternalApi;
 import org.springframework.stereotype.Component;
 
@@ -21,7 +21,7 @@ import java.util.Currency;
 public class SupportedExternalApiAndCurrencyValidator
         implements ConstraintValidator<SupportedCurrencyByExternalApi, Object[]> {
 
-    private final RateService rateService;
+    private final CurrencyService currencyService;
 
     @Override
     public boolean isValid(Object[] bankAndCurrency, ConstraintValidatorContext context) {
@@ -35,7 +35,7 @@ public class SupportedExternalApiAndCurrencyValidator
 
         ExternalApiName externalApiName = extractExternalApiName(bankAndCurrency, ExternalApiName.class);
         Currency currency = extractExternalApiName(bankAndCurrency, Currency.class);
-        return rateService.getAvailableCurrenciesByExternalApi(externalApiName).contains(currency);
+        return currencyService.isCurrencySupportedByExternalApi(externalApiName, currency);
     }
 
     private <T> T extractExternalApiName(Object[] bankAndCurrency, Class<T> aa) {
