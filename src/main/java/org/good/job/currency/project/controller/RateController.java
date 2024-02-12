cdbfeafebd3ddb1e5b44a744f4a1b2b5c390b9d0
SergeyAnshin <anshin.serega@gmail.com>
2024-02-13 00:57:11 +0300

    add statistics endpoint to RateController

--- src/main/java/org/good/job/currency/project/controller/RateController.java
+++ src/main/java/org/good/job/currency/project/controller/RateController.java
@@ -1,10 +1,12 @@
 package org.good.job.currency.project.controller;
 
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.RequiredArgsConstructor;
 import org.good.job.currency.project.entity.GeneralRate;
+import org.good.job.currency.project.entity.RateStatisticData;
 import org.good.job.currency.project.entity.enums.ExternalApiName;
-import org.good.job.currency.project.service.impl.RateServiceImpl;
+import org.good.job.currency.project.service.RateService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,17 +23,26 @@ import java.util.Currency;
 @Validated
 
 @RestController
-@RequestMapping("/rate")
+@RequestMapping("/rates")
 public class RateController {
 
-    private final RateServiceImpl rateServiceImpl;
+    private final RateService rateService;
 
     @GetMapping
     public ResponseEntity<GeneralRate> getExternalApiCurrencyRateByDate(@RequestParam ExternalApiName externalApiName,
                                                                         @RequestParam Currency currency,
                                                                         @RequestParam @PastOrPresent LocalDate date) {
-        var rate = rateServiceImpl.getRateByExternalApiNameAndCurrencyAndDate(externalApiName, currency, date);
+        var rate = rateService.getRateByExternalApiNameAndCurrencyAndDate(externalApiName, currency, date);
         return ResponseEntity.ok(rate);
+    }
+
+    @GetMapping("/statistics")
+    public ResponseEntity<RateStatisticData> getCurrencyRateStatistics(@RequestParam ExternalApiName externalApiName,
+                                                                       @RequestParam String currency,
+                                                                       @RequestParam @Past LocalDate startDate,
+                                                                       @RequestParam @PastOrPresent LocalDate endDate) {
+        var rateStatistics = rateService.getStatistics(externalApiName, currency, startDate, endDate);
+        return ResponseEntity.ok(rateStatistics);
     }
 
 }
