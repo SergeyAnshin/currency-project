d517004289828e1b65ee4cc7f560b759c977e782
SergeyAnshin <anshin.serega@gmail.com>
2024-02-12 20:28:05 +0300

    add date field to GeneralRate

--- src/main/java/org/good/job/currency/project/entity/GeneralRate.java
+++ src/main/java/org/good/job/currency/project/entity/GeneralRate.java
@@ -3,6 +3,7 @@ package org.good.job.currency.project.entity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 
@@ -13,8 +14,10 @@ import java.util.Objects;
 @Setter
 @ToString
 public class GeneralRate {
+
     private double buyRate;
     private double sellRate;
+    private LocalDate date;
 
     @Override
     public boolean equals(Object o) {
@@ -22,12 +25,12 @@ public class GeneralRate {
         if (o == null || getClass() != o.getClass()) return false;
         GeneralRate that = (GeneralRate) o;
         return Double.compare(buyRate, that.buyRate) == 0
-                && Double.compare(sellRate, that.sellRate) == 0;
+                && Double.compare(sellRate, that.sellRate) == 0 && Objects.equals(date, that.date);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(buyRate, sellRate);
+        return Objects.hash(buyRate, sellRate, date);
     }
 
 }
