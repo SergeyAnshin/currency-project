c96c592c5d176c6b9e37b5cdff4687281c5d530d
SergeyAnshin <anshin.serega@gmail.com>
2024-02-17 00:45:22 +0300

    some changes

--- src/main/java/org/good/job/currency/project/controller/CurrencyController.java
+++ src/main/java/org/good/job/currency/project/controller/CurrencyController.java
@@ -5,9 +5,17 @@ import org.good.job.currency.project.entity.enums.ExternalApiName;
 import org.good.job.currency.project.service.impl.CurrencyServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
+/**
+ * Del Validated
+ * Change CurrencyServiceImpl to interface
+ * Change ResponseEntity<?> to  ResponseEntity<List<CurrencyCodeDto>>
+ */
 @RequiredArgsConstructor
 
 @Validated
--- src/test/java/org/good/job/currency/project/service/mapper/RateMapperTest.java
+++ src/test/java/org/good/job/currency/project/service/mapper/RateMapperTest.java
@@ -1,29 +1,23 @@
 package org.good.job.currency.project.service.mapper;
 
-import org.good.job.currency.project.dto.AlfaBankRate;
-import org.good.job.currency.project.dto.BelarusBankRate;
-import org.good.job.currency.project.dto.NbrbRateDto;
+import org.good.job.currency.project.dto.AlfaBankDto;
+import org.good.job.currency.project.dto.BelarusBankDto;
+import org.good.job.currency.project.dto.NationalBankDto;
 import org.good.job.currency.project.entity.ExternalApiUrl;
 import org.good.job.currency.project.entity.GeneralRate;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Currency;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
 class RateMapperTest {
-
-    @Mock
-    private BelarusBankRateMapperImpl belarusBankRateMapper;
 
     @InjectMocks
     private RateMapperImpl rateMapper;
@@ -33,12 +27,12 @@ class RateMapperTest {
         var externalApiUrl = ExternalApiUrl.builder().build();
         var sellRate = 3.38;
         var buyRate = 3.53;
-        var alfaBankExternalApiDto = AlfaBankRate.builder()
+        var alfaBankExternalApiDto = AlfaBankDto.builder()
                 .sellRate(sellRate)
-                .sellIso("EUR")
+                .sellCurrencyCode("EUR")
                 .sellCode(978)
                 .buyRate(buyRate)
-                .buyIso("BYN")
+                .buyCurrencyCode("BYN")
                 .buyCode(933)
                 .quantity(1)
                 .name("евро")
@@ -54,10 +48,10 @@ class RateMapperTest {
         var externalApiUrl = ExternalApiUrl.builder().build();
         var sellRate = 3.2253;
         var buyRate = 0;
-        var nbrbExternalApiDto = NbrbRateDto.builder()
+        var nbrbExternalApiDto = NationalBankDto.builder()
                 .currencyId(431)
                 .date(LocalDateTime.of(2024, 2, 9, 0, 0))
-                .currencyAbbreviation("USD")
+                .sellCurrencyCode("USD")
                 .foreignCurrencyUnitsNumber(1)
                 .currencyName("Доллар США")
                 .sellRate(sellRate)
@@ -70,10 +64,10 @@ class RateMapperTest {
 
     @Test
     void mapperCorrectlyMapsRatesForBelarusBank() {
-        var externalApiUrl = ExternalApiUrl.builder().currency(Currency.getInstance("USD")).build();
+        var externalApiUrl = ExternalApiUrl.builder().currency("USD").build();
         var sellRate = 3.245;
         var buyRate = 3.185;
-        var belarusBankRateExternalApiDto = BelarusBankRate.builder()
+        var belarusBankRateExternalApiDto = BelarusBankDto.builder()
                 .date(LocalDateTime.of(2024, 2, 9, 16, 30))
                 .buyUsd(buyRate).sellUsd(sellRate).buyEur(3.4).sellEur(3.49).buyRub(3.47).sellRub(3.54).buyCny(4.42)
                 .sellCny(4.54).buyConversionUsdEur(0.92).sellConversionUsdEur(1.065).buyConversionUsdRub(90.30)
@@ -81,9 +75,6 @@ class RateMapperTest {
                 .buyConversionCnyUsd(0.1365).sellConversionCnyUsd(7.0155).buyConversionCnyEur(0.1270)
                 .sellConversionCnyEur(7.4890).buyConversionCnyRub(12.4860).sellConversionCnyRub(0.0765).build();
         var belarusBankGeneralRateResult = GeneralRate.builder().buyRate(buyRate).sellRate(sellRate).build();
-
-        when(belarusBankRateMapper.externalApiRateDtoToRate(belarusBankRateExternalApiDto, externalApiUrl))
-                .thenReturn(belarusBankGeneralRateResult);
 
         var generalRate = rateMapper.externalApiRateDtoToRate(belarusBankRateExternalApiDto, externalApiUrl);
         assertEquals(belarusBankGeneralRateResult, generalRate);
